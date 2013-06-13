8fe4bd97b69919f81a5d9ea103942176ddeafca0
davemuir <davidmuirdesign@gmail.com>
2013-06-13 17:37:06 -0400

    reverted

--- wp-content/themes/twentytwelve/index.php
+++ wp-content/themes/twentytwelve/index.php
@@ -16,15 +16,12 @@
 
 get_header(); ?>
 	
-	<?php build_i_world_map(1);
+	<?php build_i_world_map(1); 	?> 
 	<script>
 	if(ivalue_1[selectedRegion]=="CA"){
 		alert('true');
 	}
 	</script>
-	
-	 	?> 
-	
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
