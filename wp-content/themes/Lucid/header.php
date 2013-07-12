e2e942eb171ff19adb547dcbde90e36c74bef5f0
davemuir <davidmuirdesign@gmail.com>
2013-07-12 13:47:03 -0400

    added header query file

--- wp-content/themes/Lucid/header.php
+++ wp-content/themes/Lucid/header.php
@@ -22,7 +22,7 @@
 
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
+	
 	<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 	<![endif]-->
@@ -30,7 +30,7 @@
 	<script type="text/javascript">
 		document.documentElement.className = 'js';
 	</script>
-
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
