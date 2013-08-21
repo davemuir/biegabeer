5d774f41e0bf16e5f4e00868dffd845168f5624f
davemuir <davidmuirdesign@gmail.com>
2013-08-21 16:40:57 -0400

    added a real image for button

--- wp-content/themes/Lucid/home.php
+++ wp-content/themes/Lucid/home.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
- <a id="mapBack" href="#">back</a>   
+ <img id="mapBack" src="http://biegamanmaptest.herokuapp.com/wp-content/themes/Lucid/images/backButton.png">
  <div id="atlasMap"><?php build_i_world_map(1); ?></div>
  <div id="canadaMap"><?php build_i_world_map(11);?> </div> 
  <div id="argentinaMap"><?php build_i_world_map(21);?> </div> 		
