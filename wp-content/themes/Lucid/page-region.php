14b1c049b1822575a64d9515735721f73d340ec3
davemuir <davidmuirdesign@gmail.com>
2013-08-27 15:31:47 -0400

    wrapped with loop p[age

--- wp-content/themes/Lucid/page-region.php
+++ wp-content/themes/Lucid/page-region.php
@@ -9,13 +9,66 @@ Template Name: Pages by Region
 	
 	<div id="left-area">
 		<?php get_template_part('includes/breadcrumbs', 'page'); ?>
-		<?php get_template_part('loop', 'page'); ?>
-		
-			<?php 
-		$variable = $_GET['var'];
-		echo $variable;
-
+<!--start wrap-->
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class('entry clearfix'); ?>>
+		<?php
+			$thumb = '';
+			$width = (int) apply_filters('et_blog_image_width',630);
+			$height = (int) apply_filters('et_blog_image_height',210);
+			$classtext = '';
+			$titletext = get_the_title();
+			$thumbnail = get_thumbnail($width,$height,$classtext,$titletext,$titletext,false,'Singleimage');
+			$thumb = $thumbnail["thumb"];
 		?>
+		<?php if ( '' != $thumb && 'on' == et_get_option('lucid_page_thumbnails') ) { ?>
+			<div class="post-thumbnail">
+				<?php print_thumbnail($thumb, $thumbnail["use_timthumb"], $titletext, $width, $height, $classtext); ?>
+			</div> 	<!-- end .post-thumbnail -->
+		<?php } ?>
+				<div class="post_content clearfix">
+			<h1 class="title"><?php the_title(); ?></h1>
+<!--end wrap for original gets-->		
+<?php 
+	$variable = $_GET['var'];
+	
+		
+
+	$args = array();
+
+	$myposts = get_posts( $args );
+	foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+		<?php 
+	
+		$postID = get_the_ID();
+		$postRegionVar = get_post_meta($postID, "regionVar", true );
+	
+	 	?>
+			<?php 
+			if($variable == $postRegionVar){ 
+			?>
+			<li><a href="<?php post_permalink($postID) ?>"> <?php the_title($postID) ?></a></li>
+		
+	 		<?php } ?>
+		
+	
+	
+	<?php endforeach; 
+	wp_reset_postdata();?>
+
+
+<!--start wrap again-->
+		
+<?php the_content(); ?>
+			<?php wp_link_pages(array('before' => '<p><strong>'.esc_attr__('Pages','Lucid').':</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+			<?php edit_post_link(esc_attr__('Edit this page','Lucid')); ?>			
+				</div> 	<!-- end .post_content -->
+	</article> <!-- end .entry -->
+<?php endwhile; // end of the loop. ?>	
+		
+<!--end wrap again-->		
+		
+		
 		<?php if ( 'on' == et_get_option('lucid_show_pagescomments') ) comments_template('', true); ?>
 		
 		
