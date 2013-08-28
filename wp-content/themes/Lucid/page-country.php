9a50fe4e1a32a1d7d4612a73bed7247fc2c82e02
davemuir <davidmuirdesign@gmail.com>
2013-08-28 17:13:20 -0400

    added in a new template argue,ent

--- wp-content/themes/Lucid/page-country.php
+++ wp-content/themes/Lucid/page-country.php
@@ -28,7 +28,83 @@ Template Name: Pages by Country
 		<?php } ?>
 				<div class="post_content clearfix">
 			<h1 class="title"><?php the_title(); ?></h1>
-<!--end wrap for original gets-->		
+			<div class="alphaSelector">
+			<a href="#a">A</a>
+			<a href="#b">B</a>
+			<a href="#c">C</a>
+			<a href="#d">D</a>
+			<a href="#e">E</a>
+			<a href="#f">F</a>
+			<a href="#g">G</a>
+			<a href="#h">H</a>
+			<a href="#i">I</a>
+			<a href="#j">J</a>
+			<a href="#k">K</a>
+			<a href="#l">L</a>
+			<a href="#m">M</a>
+			<a href="#n">N</a>
+			<a href="#o">O</a>
+			<a href="#p">P</a>
+			<a href="#q">Q</a>
+			<a href="#r">R</a>
+			<a href="#s">S</a>
+			<a href="#t">T</a>
+			<a href="#u">U</a>
+			<a href="#v">V</a>
+			<a href="#w">W</a>
+			<a href="#x">X</a>
+			<a href="#y">Y</a>
+			<a href="#z">Z</a>
+			</div>
+<!--end wrap for original gets country terms-->		
+
+<ul>
+<h3 id="a" class="byCountryLetter" >A</h3>
+<?php $args = array(
+	'posts_per_page'   => 5,
+	'offset'           => 0,
+	'category'         => '',
+	'orderby'          => 'Country',
+	'order'            => 'ASC',
+	'include'          => '',
+	'exclude'          => '',
+	'meta_key'         => '',
+	'meta_value'       => '',
+	'post_type'        => 'post',
+	'post_mime_type'   => '',
+	'post_parent'      => '',
+	'post_status'      => 'publish',
+	'suppress_filters' => true ); ?>
+<?php	
+	$myposts = get_posts( $args );
+	foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+		<?php 
+	
+		$postID = get_the_ID();
+		$postRegionVar = get_post_meta($postID, "Country", true );
+		$variable = substr($postRegionVar, 0, 1);
+		$once = 1;
+	 	?>
+			
+			<?php 
+			if($variable == "A" && $once = 1 && $setRegion != $postRegionVar){ 
+				$setRegion = $postRegionVar;
+				$once = 2;
+			?>
+				<h3 class="byCountryName"> <?php echo $postRegionVar ?> </h3>
+				
+		
+	 	<?php }if($variable == "A" ){
+				$permalink = get_permalink($postID);
+				$title = get_the_title($postID);	
+			 ?>		
+			<li><a href="<?php echo $permalink ?>"> <?php echo $title ?></a></li>
+		
+	 	<?php } ?>
+
+	<?php endforeach; 
+	wp_reset_postdata();?>
+</ul>			
 
 <!--start wrap again-->
 		
