1d15d803331345ad8891fbe370cc1373bb3d7afd
davemuir <davidmuirdesign@gmail.com>
2013-09-09 15:17:38 -0400

    more edited sort pages

--- wp-content/themes/Lucid/page-style.php
+++ wp-content/themes/Lucid/page-style.php
@@ -28,18 +28,87 @@ Template Name: Pages by Style
 		<?php } ?>
 				<div class="post_content clearfix">
 			<h1 class="title"><?php the_title(); ?></h1>
-			<?php the_content(); ?>
-<!--end wrap for original gets-->		
+			<?php the_content(); 
+	?>
+<div class="alphaSelector">
+			<a href="#a">A</a>
+			<a href="#b">B</a>
+			<a href="#c">C</a>
+			<a href="#d">D</a>
+			<a href="#e">E</a>
+			<a href="#f">F</a>
+			<a href="#g">G</a>
+			<a href="#h">H</a>
+			<a href="#i">I</a>
+			<a href="#j">J</a>
+			<a href="#k">K</a>
+			<a href="#l">L</a>
+			<a href="#m">M</a>
+			<a href="#n">N</a>
+			<a href="#o">O</a>
+			<a href="#p">P</a>
+			<a href="#q">Q</a>
+			<a href="#r">R</a>
+			<a href="#s">S</a>
+			<a href="#t">T</a>
+			<a href="#u">U</a>
+			<a href="#v">V</a>
+			<a href="#w">W</a>
+			<a href="#x">X</a>
+			<a href="#y">Y</a>
+			<a href="#z">Z</a>
+			</div>
+<!--end wrap for original gets-->
+
+<h3 id="a" class="byCountryLetter" >A</h3>
+<ul>
 <?php
 $args = array(
   'orderby' => 'name',
   'parent' => 0
   );
 $categories = get_categories( $args );
-foreach ( $categories as $category ) {
-	echo '<a href="' . get_category_link( $category->term_id ) . '"><h4 class="beerStyle">' . $category->name . '</h4></a><br/>';
+
+
+
+foreach ( $categories as $category) { 
+	
+	$catName = $category->name;
+	$categoryLetter = substr($catName, 0, 1);
+
+	if( $categoryLetter == "A"){
+	echo '<li><a href="' . get_category_link( $category->term_id ) . '"><h4 class="beerStyle">' . $category->name . '</h4></a></li>';
+	}
+
 }
 ?>
+</ul>		
+<h3 id="a" class="byCountryLetter" >B</h3>
+<ul>
+<?php
+$args = array(
+  'orderby' => 'name',
+  'parent' => 0
+  );
+$categories = get_categories( $args );
+
+
+
+foreach ( $categories as $category) { 
+	
+	$catName = $category->name;
+	$categoryLetter = substr($catName, 0, 1);
+
+	if( $categoryLetter == "B"){
+	echo '<li><a href="' . get_category_link( $category->term_id ) . '"><h4 class="beerStyle">' . $category->name . '</h4></a></li>';
+	}
+
+}
+?>
+</ul>		
+
+
+
 
 <!--start wrap again-->
 		
