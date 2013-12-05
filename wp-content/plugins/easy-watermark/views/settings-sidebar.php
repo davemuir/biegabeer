4dc054cd2c2e12aaa5ce1d3217baa50ecfd25195
davemuir <davidmuirdesign@gmail.com>
2013-12-05 14:25:58 -0500

    water mark plug ins

---
+++ wp-content/plugins/easy-watermark/views/settings-sidebar.php
@@ -0,0 +1,5 @@
+		<div class="metabox-holder inner-sidebar">
+			<?php include dirname(__FILE__) . EWDS . 'about.php'; ?>
+			<?php if($current_tab == 'text') 
+				include dirname(__FILE__) . EWDS . 'placeholders.php'; ?>
+		</div><!-- .metabox-holder -->
