b739dcf7442bb507e5da7b4df878d3e09cc06d64
davemuir <davidmuirdesign@gmail.com>
2013-12-06 14:41:22 -0500

    footer/styles

--- wp-content/themes/Lucid/footer.php
+++ wp-content/themes/Lucid/footer.php
@@ -2,42 +2,11 @@
 	$footer_sidebars = array('footer-area-1','footer-area-2','footer-area-3');
 	$any_widget_area_active = is_active_sidebar( $footer_sidebars[0] ) || is_active_sidebar( $footer_sidebars[1] ) || is_active_sidebar( $footer_sidebars[2] );
 ?>
-
-		</div> <!-- end .container -->
-	</div> <!-- end #main-area -->
-	<footer id="main-footer">
-	<?php if ( $any_widget_area_active ) { ?>
-		<div id="footer-divider"></div>
-	<?php } ?>
-		<div class="container">
-			<div id="footer-widgets" class="clearfix">
-				<?php
-					if ( $any_widget_area_active ) {
-						foreach ( $footer_sidebars as $key => $footer_sidebar ){
-							if ( is_active_sidebar( $footer_sidebar ) ) {
-								echo '<div class="footer-widget' . (  2 == $key ? ' last' : '' ) . '">';
-								dynamic_sidebar( $footer_sidebar );
-								echo '</div> <!-- end .footer-widget -->';
-							}
-						}
-					}
-				?>
-			</div> <!-- end #footer-widgets -->
-		</div> <!-- end .container -->
-
-		<?php if ( 'on' == et_get_option( 'lucid_728_enable', 'false' ) ){ ?>
-			<div id="bottom-advertisment">
-				<div class="container">
-					<?php
-						if ( ( $lucid_728_adsense = et_get_option('lucid_728_adsense') ) && '' != $lucid_728_adsense ) echo( $lucid_728_adsense );
-						else { ?>
-						   <a href="<?php echo esc_url(et_get_option('lucid_728_url')); ?>"><img src="<?php echo esc_url(et_get_option('lucid_728_image')); ?>" /></a>
-					<?php } ?>
-				</div> <!-- end .container -->
-			</div>
-		<?php } ?>
-	<div class="container">
+<div class="container">
+			
 		<div class="footerSocial">
+			<h3 class="midText">What's it all About?</h3>
+		<p class="midText">Biega Beer is founded by gastronome Jan Biega and is dedicated to exploring the culture and pleasures of beer.  This website features  reviews of more than 2500 beers from more than 700 breweries and 80 countries.  Biega Beer specializes in consulting and strategic development for craft breweries and has extensively researched online beer communities. </p>
 			<div class="socialButtons">
 			<a href="#" id="facebookSocial"><img src="http://biegamanmaptest.herokuapp.com/wp-content/themes/Lucid/images/facebookSocial.png"></a>
 			<a href="#" id=""><img src="http://biegamanmaptest.herokuapp.com/wp-content/themes/Lucid/images/twitterSocial.png"></a>
@@ -46,9 +15,11 @@
 			</div>
 		</div>
 	</div>
-	</footer> <!-- end #main-footer -->
+		</div> <!-- end .container -->
+	</div> <!-- end #main-area -->
+	
 
-
-	<?php wp_footer(); ?>
+<?php wp_footer(); ?>
+	
 </body>
 </html>
