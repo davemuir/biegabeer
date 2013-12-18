9d18374cf6d62a9dff5f01449a154c7df74ac624
davemuir <davidmuirdesign@gmail.com>
2013-12-18 16:02:01 -0500

    header-101

--- wp-content/themes/Lucid/page.php
+++ wp-content/themes/Lucid/page.php
@@ -1,6 +1,11 @@
+<?php
+$page_object = get_queried_object();
+$page_id     = get_queried_object_id();
+?>
 <?php get_header(); ?>
 
 <div id="content-area" class="clearfix">
+<?php echo $page_id;?>
 	<div id="left-area">
 		<?php get_template_part('includes/breadcrumbs', 'page'); ?>
 		<?php get_template_part('loop', 'page'); ?>
